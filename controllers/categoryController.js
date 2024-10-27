c9a77c675cd68b836627a6e89557407fdb4707a2
suneelfm <sfm.20031998@gmail.com>
2024-10-27 14:15:14 +0530

    Handled pagination

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,26 @@
 import { Category } from "../models/categories.js";
 
-export const fetchCategories = (req, res, next) => {
+export const fetchCategories = async (req, res, next) => {
   try {
+    const page = parseInt(req.query.page) ?? 1;
+    const limit = parseInt(req.query.limit) ?? 6;
+
+    const skip = (page - 1) * limit;
+
+    const total = await Category.countDocuments();
+
     Category.find()
+      .skip(skip)
+      .limit(limit)
+      .exec()
       .then((data) => {
-        res.send({ data, message: "Categories fetched successfully." });
+        res.send({
+          total,
+          page,
+          categories: data,
+          totalPages: Math.ceil(total / limit),
+          message: "Categories fetched successfully.",
+        });
       })
       .catch((error) => {
         res.status(500).send({ message: "Something went wrong." });
